9cb6ceb2777faa734d4d1f2c959bec394442eb3f
Brian <brianrik911@gmail.com>
2023-08-03 18:34:23 +0300

    code bytes complete

--- assembler.c
+++ assembler.c
@@ -83,30 +83,70 @@ short get_opcode_index(const char *opcode) {
     printf("Opcode not found: %s\n", opcode);
     return -1;
 }
-void parse_elements(const char *opcode, const char *element1, const char *element2, int row, int code_type) {
 
+
+short element_number(const char* str) {
+    if (str[0] == '@') {
+        // The string starts with '@', so we return the last character as a number.
+        return (short)str[3];
+    }
+    else if (isdigit(str[0])) {
+        // The string is a number, so we convert it to a short int and return it.
+        return (short)atoi(str);
+    }
+    else {
+        // We search for the string in the labels array and return the corresponding row_id.
+        for (int i = 0; i < labels.size; i++) {
+            if (strcmp(labels.array[i], str) == 0) {
+                return (short)labels.row_ids[i];
+            }
+        }
+    }
+    // If the string is not found in the labels array, we return -1.
+    return -1;
+}
+
+
+
+void parse_elements(const char *opcode, const char *element1, const char *element2, int code_type) {
 
     short int num = 0;
+    short int element1_number = 0;
+    short int element2_number = 0;
 
     short int element1_type, element2_type, opcode_type;
     if(element1 == 0)
         element1_type = 0;
     else
         element1_type = element_type(element1);
+        element1_number = element_number(element1);
     if(element2 == 0)
         element2_type = 0;
     else
         element2_type = element_type(element2);
+        element2_number = element_number(element2);
 
     opcode_type = get_opcode_index(opcode);
-
 
     replace_bits(&num, 0,1,code_type);
     replace_bits(&num,2,4,element2_type);
     replace_bits(&num,5,8,opcode_type);
     replace_bits(&num,9,11,element1_type);
     print_bits(num);
-    printf("s");
+
+    if(element1_number != 0){
+        short int num1 = 0;
+        replace_bits(&num1, 2, 11, element1_number);
+        print_bits(num1);
+    }
+    if(element2_number != 0){
+        short int num2 = 0;
+        replace_bits(&num2, 2, 11, element2_number);
+        print_bits(num2);
+    }
+
+
+
 
 
 
@@ -146,7 +186,7 @@ void process_am_file(const char* filename) {
                 }
 
                 // Parse the elements
-                parse_elements(word, element1, element2, row, 0);
+                parse_elements(word, element1, element2,  0);
             }
             word = strtok(NULL, " ");
         }
